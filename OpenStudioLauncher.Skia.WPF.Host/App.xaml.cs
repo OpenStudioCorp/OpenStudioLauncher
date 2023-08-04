5f5b409990e117e31a39d64e4d95041127a71fc3
Arif O <frodggyxd@gmail.com>
2023-08-04 02:52:22 -0400

    init commit for linux port with uno

--- OpenStudioLauncher.Skia.WPF.Host/App.xaml.cs
+++ OpenStudioLauncher.Skia.WPF.Host/App.xaml.cs
@@ -6,12 +6,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace OpenStudioLauncher
+namespace OpenStudioLauncher.WPF.Host
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
 }
---
+++ OpenStudioLauncher.Skia.WPF/Program.cs
@@ -0,0 +1,3 @@
+﻿namespace OpenStudioLauncher.Skia.Gtk
+{
+}
