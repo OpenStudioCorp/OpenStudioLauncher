9ca4512e84c0eed4c42c258be261e923a46698de
Charlie <gamerboy030607@gmail.com>
2023-08-18 06:53:09 +1000

    upload new launcher for first time

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,3 +1,8 @@
+// This Source Code Form is subject to the terms of the MIT License.
+// If a copy of the MIT was not distributed with this file, You can obtain one at https://opensource.org/licenses/MIT.
+// Copyright (C) Leszek Pomianowski and WPF UI Contributors.
+// All Rights Reserved.
+
 using System.Windows;
 
 [assembly: ThemeInfo(
@@ -5,6 +10,6 @@ using System.Windows;
                                      //(used if a resource is not found in the page,
                                      // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-                                              //(used if a resource is not found in the page,
-                                              // app, or any theme specific resource dictionaries)
+//(used if a resource is not found in the page,
+// app, or any theme specific resource dictionaries)
 )]
