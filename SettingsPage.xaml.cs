f9df411401fdd0b35ecefa6aae43b57f969a550b
charlie_sans <gamerboy030607@gmail.com>
2023-09-24 22:29:51 +1000

    update OpenStudioLauncher

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
-
+using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using Microsoft.Maui.Controls.Xaml;
 namespace MauiApp2
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
@@ -25,14 +27,11 @@ namespace MauiApp2
 						string[] parts = line.Split('=');
 						if (parts.Length == 2)
 						{
-							if (parts[0] == "SoundEnabled")
-							{
-								SoundSwitch.IsToggled = bool.Parse(parts[1]);
-							}
-							else if (parts[0] == "NotificationsEnabled")
-							{
-								NotificationsSwitch.IsToggled = bool.Parse(parts[1]);
-							}
+							
+							NotificationsSwitch.IsToggled = parts[0] == "NotificationsEnabled" ? bool.Parse(parts[1]) : NotificationsSwitch.IsToggled;
+							AppSoundsSwitch.IsToggled = parts[0] == "AppSoundsEnabled" ? bool.Parse(parts[1]) : AppSoundsSwitch.IsToggled;
+							UpdateNotificationsSwitch.IsToggled = parts[0] == "UpdateNotificationsEnabled" ? bool.Parse(parts[1]) : UpdateNotificationsSwitch.IsToggled;
+							LanguagePicker.SelectedItem = parts[0] == "SelectedLanguage" ? parts[1] : LanguagePicker.SelectedItem;
 						}
 					}
 				}
@@ -42,8 +41,12 @@ namespace MauiApp2
 		private void OnSaveClicked(object sender, EventArgs e)
 		{
 			// Save the sound and notifications settings
-			bool soundEnabled = SoundSwitch.IsToggled;
+			bool soundEnabled = AppSoundsSwitch.IsToggled;
 			bool notificationsEnabled = NotificationsSwitch.IsToggled;
+            bool appSoundsEnabled = AppSoundsSwitch.IsToggled;
+            bool updateNotificationsEnabled = UpdateNotificationsSwitch.IsToggled;
+            string selectedLanguage = LanguagePicker.SelectedItem.ToString();
+			
 
 			// Save the settings to a file
 			string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "settings.txt");
@@ -51,6 +54,9 @@ namespace MauiApp2
 			{
 				writer.WriteLine($"SoundEnabled={soundEnabled}");
 				writer.WriteLine($"NotificationsEnabled={notificationsEnabled}");
+				writer.WriteLine($"AppSoundsEnabled={appSoundsEnabled}");
+				writer.WriteLine($"UpdateNotificationsEnabled={updateNotificationsEnabled}");
+				writer.WriteLine($"SelectedLanguage={selectedLanguage}");
 			}
 		}
 	}
